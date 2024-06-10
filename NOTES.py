9b508f90386ef71a3a05c9a6769c06ad4cd928ec
Mana <86923626+Mana42138@users.noreply.github.com>
2024-06-10 23:34:18 +0200

    Delete NOTES.py

--- NOTES.py
+++
@@ -1,7 +0,0 @@
-"""
-	This is a Notes file
-    
-    THIS WILL ONLY BE USED TO GAIN INFORMATION ABOUT THE PYTHON API.
-"""
-
-# os.path.dirname -> can get the parrenmt of any folder (YOU NEED THE PATH!)
